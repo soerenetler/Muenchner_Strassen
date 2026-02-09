f0a462b6718ed6583f085cd7ad037e00453563ee
Sören Etler <moin@soerenetler.de>
2026-02-09 20:34:09 +0100

    remove datastory from streamlit

--- pages/Data_Story.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-file_path = 'storytelling-main/index.html'
-
-with open(file_path, 'r') as file:
-    file_content = file.read()
-
-st.html(file_content, unsafe_allow_javascript=True)
